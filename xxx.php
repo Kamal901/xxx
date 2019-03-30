e56bf2dab76b84765af99e41e9c9dd8d18fcac6e
Kamal901 <31289533+Kamal901@users.noreply.github.com>
2019-03-30 11:12:32 +0700

    Update xxx.php

--- xxx.php
+++ xxx.php
@@ -8,16 +8,14 @@ $red="\033[1;31m";
 $kuning="\033[1;33m";
 
 //flag
-print"$ijo
-===================================================\n$red
-=       Samehikari Music - Bugsar Chanel          =$putih
+print"$pink
 ===================================================
-               $red  !!!! WARNING !!!!$pink
-              $red ==> KOTA Bot Ilegal <==$pink
-                $kuning bisa kena $red Banned$pink
-              $pink pergunakan dengan bijak$ijo
-                 $biru Te$turkis ri$ijo ma$pink ka$kuning sih\n$ijo
-===================================================\n$biru";
+               $red  !!!! WARNING !!!!
+           $pink==> KOTA Bot Ilegal <==$pink
+                $kuning bisa kena $red Banned
+              $pink pergunakan dengan bijak
+===================================================\n
+$biru";
 function id($sign,$data,$body,$jumlah,$wait) {
 	$x = 0; 
 	while($x < $jumlah) {
@@ -37,10 +35,10 @@ function id($sign,$data,$body,$jumlah,$wait) {
 	} 
 }  
 
-echo "Jeda =(ex:0)\nInput : " echo $wait = "40";
-echo "Sign =\nInput : "  echo $token = "NzMzNzk4MWI5M2E5YjQwM2MwNGVkNGM4NTFlYzgzOGIzMDVmNGM2Yg==";
-echo "Data =\nInput : "  echo $token = "bc3DdKx0xy0IjAH4NBenYq9sLPDnqhuua6I36M8Z96MZylzKE4u2L6cE2%2FIRsuk5ouab%2FoNfRVx0tNEbJDG0LXPHGd6WNtF%2FG9nx%2FO5S%2Bj7e9tP2Ul1PmNWs6q8HIs0EZdZwT4WtM5Je9VfiNp7gm8MkEyKArLUqCxWkmc6leSi9HJB9nfGeSvSlIDDdoEUJeNChru9AGR4FEIB6LRWvltmzLHfWvIvOxRD%2Bnj96bSEHaHOeu8u8FgMyvquk5jAv7Vu4EVF4qPDprm%2BUXNtr38Z%2B5sOQEX7ep5eOeTpDtBA0lt4dnBv6pjYD8BcpOPuJ580v7dcs8XzUb7cROj2Ub36WnMZEviBWIoRuGWkyv8RTOX8L1ufrrrTSycHMQzLHIsZpP9VbFua45xpLHuD1US0g1pMuFqvnUf1gVUq72FaumPQXzmWPJDohQp%2BpRaTcoRMcOc01x4YiXZvbA71B0njYcCZpE5tHKI085WoPH9uLrB91J1n0DSumIEu0cA9tqNsWjUSsfUSx9Jxmpu9CHa%2FMzv%2BUNssP%2Bx1iaGzZtRR1Ss535MZWfmsiEgvsB%2BBfbHUBMiT7elorG4QyO02rGOwP8NgIf61Y%2BKsLpPfJuVAPl1o0YM4VRaja%2Fg60N0Us0ZKXfmMD0z9LU%2F85NA8kY0TvudtplmZ7m0toAXyCSu8zworlAV1qO5gEEuXOmwlejVFFP0mYkgwNRVVli07%2BNXv8xlSPPw1m0TM8SX7iyQf0al9wiSoH0VkqWZcQpJ6J2YdcRlOs4CcEihPgTQ4xAtcIBupT%2FQRqtMQh7fzYiJB0Ihh9We7d8TwKqADNyNqpVZ6TiTOCdelWQ36YyhBqZi0t%2FBf6o%2FZvWHgEYAf1MCMY2fKM%2FNMCxDQXr902IOondaJWAg1d8hE0Y4p135gF4KScLOnRbxn7n9u%2FbfrVz0s%3D&key=n6Z8qI2UQFZ3M%2BIPaFgpeuIy4tsr0%2F4a%2F%2FQYfev3m1nW9YuYd1ZXWo9aitVW%2Bg4ba2x%2FIaLCfv6T2%2BC6RQtXAUSEXUs4yvsCOrYUiCru%2FbKAikSUvontF7NkZYohiJMbJIP1zAEIPnN6fY%2Foh0rOL4GtNoeQeWEHqzO3PZsdYIs%3D";
-echo "Body =\nInput : "  echo $token = "sign=NzMzNzk4MWI5M2E5YjQwM2MwNGVkNGM4NTFlYzgzOGIzMDVmNGM2Yg==&data=bc3DdKx0xy0IjAH4NBenYq9sLPDnqhuua6I36M8Z96MZylzKE4u2L6cE2%2FIRsuk5ouab%2FoNfRVx0tNEbJDG0LXPHGd6WNtF%2FG9nx%2FO5S%2Bj7e9tP2Ul1PmNWs6q8HIs0EZdZwT4WtM5Je9VfiNp7gm8MkEyKArLUqCxWkmc6leSi9HJB9nfGeSvSlIDDdoEUJeNChru9AGR4FEIB6LRWvltmzLHfWvIvOxRD%2Bnj96bSEHaHOeu8u8FgMyvquk5jAv7Vu4EVF4qPDprm%2BUXNtr38Z%2B5sOQEX7ep5eOeTpDtBA0lt4dnBv6pjYD8BcpOPuJ580v7dcs8XzUb7cROj2Ub36WnMZEviBWIoRuGWkyv8RTOX8L1ufrrrTSycHMQzLHIsZpP9VbFua45xpLHuD1US0g1pMuFqvnUf1gVUq72FaumPQXzmWPJDohQp%2BpRaTcoRMcOc01x4YiXZvbA71B0njYcCZpE5tHKI085WoPH9uLrB91J1n0DSumIEu0cA9tqNsWjUSsfUSx9Jxmpu9CHa%2FMzv%2BUNssP%2Bx1iaGzZtRR1Ss535MZWfmsiEgvsB%2BBfbHUBMiT7elorG4QyO02rGOwP8NgIf61Y%2BKsLpPfJuVAPl1o0YM4VRaja%2Fg60N0Us0ZKXfmMD0z9LU%2F85NA8kY0TvudtplmZ7m0toAXyCSu8zworlAV1qO5gEEuXOmwlejVFFP0mYkgwNRVVli07%2BNXv8xlSPPw1m0TM8SX7iyQf0al9wiSoH0VkqWZcQpJ6J2YdcRlOs4CcEihPgTQ4xAtcIBupT%2FQRqtMQh7fzYiJB0Ihh9We7d8TwKqADNyNqpVZ6TiTOCdelWQ36YyhBqZi0t%2FBf6o%2FZvWHgEYAf1MCMY2fKM%2FNMCxDQXr902IOondaJWAg1d8hE0Y4p135gF4KScLOnRbxn7n9u%2FbfrVz0s%3D&key=n6Z8qI2UQFZ3M%2BIPaFgpeuIy4tsr0%2F4a%2F%2FQYfev3m1nW9YuYd1ZXWo9aitVW%2Bg4ba2x%2FIaLCfv6T2%2BC6RQtXAUSEXUs4yvsCOrYUiCru%2FbKAikSUvontF7NkZYohiJMbJIP1zAEIPnN6fY%2Foh0rOL4GtNoeQeWEHqzO3PZsdYIs%3D";
+echo "Jeda =(ex:0)\nInput : " echo $wait = 40;
+echo "Sign =\nInput : "  echo $token = NzMzNzk4MWI5M2E5YjQwM2MwNGVkNGM4NTFlYzgzOGIzMDVmNGM2Yg==;
+echo "Data =\nInput : "  echo $token = bc3DdKx0xy0IjAH4NBenYq9sLPDnqhuua6I36M8Z96MZylzKE4u2L6cE2%2FIRsuk5ouab%2FoNfRVx0tNEbJDG0LXPHGd6WNtF%2FG9nx%2FO5S%2Bj7e9tP2Ul1PmNWs6q8HIs0EZdZwT4WtM5Je9VfiNp7gm8MkEyKArLUqCxWkmc6leSi9HJB9nfGeSvSlIDDdoEUJeNChru9AGR4FEIB6LRWvltmzLHfWvIvOxRD%2Bnj96bSEHaHOeu8u8FgMyvquk5jAv7Vu4EVF4qPDprm%2BUXNtr38Z%2B5sOQEX7ep5eOeTpDtBA0lt4dnBv6pjYD8BcpOPuJ580v7dcs8XzUb7cROj2Ub36WnMZEviBWIoRuGWkyv8RTOX8L1ufrrrTSycHMQzLHIsZpP9VbFua45xpLHuD1US0g1pMuFqvnUf1gVUq72FaumPQXzmWPJDohQp%2BpRaTcoRMcOc01x4YiXZvbA71B0njYcCZpE5tHKI085WoPH9uLrB91J1n0DSumIEu0cA9tqNsWjUSsfUSx9Jxmpu9CHa%2FMzv%2BUNssP%2Bx1iaGzZtRR1Ss535MZWfmsiEgvsB%2BBfbHUBMiT7elorG4QyO02rGOwP8NgIf61Y%2BKsLpPfJuVAPl1o0YM4VRaja%2Fg60N0Us0ZKXfmMD0z9LU%2F85NA8kY0TvudtplmZ7m0toAXyCSu8zworlAV1qO5gEEuXOmwlejVFFP0mYkgwNRVVli07%2BNXv8xlSPPw1m0TM8SX7iyQf0al9wiSoH0VkqWZcQpJ6J2YdcRlOs4CcEihPgTQ4xAtcIBupT%2FQRqtMQh7fzYiJB0Ihh9We7d8TwKqADNyNqpVZ6TiTOCdelWQ36YyhBqZi0t%2FBf6o%2FZvWHgEYAf1MCMY2fKM%2FNMCxDQXr902IOondaJWAg1d8hE0Y4p135gF4KScLOnRbxn7n9u%2FbfrVz0s%3D&key=n6Z8qI2UQFZ3M%2BIPaFgpeuIy4tsr0%2F4a%2F%2FQYfev3m1nW9YuYd1ZXWo9aitVW%2Bg4ba2x%2FIaLCfv6T2%2BC6RQtXAUSEXUs4yvsCOrYUiCru%2FbKAikSUvontF7NkZYohiJMbJIP1zAEIPnN6fY%2Foh0rOL4GtNoeQeWEHqzO3PZsdYIs%3D;
+echo "Body =\nInput : "  echo $token = sign=NzMzNzk4MWI5M2E5YjQwM2MwNGVkNGM4NTFlYzgzOGIzMDVmNGM2Yg==&data=bc3DdKx0xy0IjAH4NBenYq9sLPDnqhuua6I36M8Z96MZylzKE4u2L6cE2%2FIRsuk5ouab%2FoNfRVx0tNEbJDG0LXPHGd6WNtF%2FG9nx%2FO5S%2Bj7e9tP2Ul1PmNWs6q8HIs0EZdZwT4WtM5Je9VfiNp7gm8MkEyKArLUqCxWkmc6leSi9HJB9nfGeSvSlIDDdoEUJeNChru9AGR4FEIB6LRWvltmzLHfWvIvOxRD%2Bnj96bSEHaHOeu8u8FgMyvquk5jAv7Vu4EVF4qPDprm%2BUXNtr38Z%2B5sOQEX7ep5eOeTpDtBA0lt4dnBv6pjYD8BcpOPuJ580v7dcs8XzUb7cROj2Ub36WnMZEviBWIoRuGWkyv8RTOX8L1ufrrrTSycHMQzLHIsZpP9VbFua45xpLHuD1US0g1pMuFqvnUf1gVUq72FaumPQXzmWPJDohQp%2BpRaTcoRMcOc01x4YiXZvbA71B0njYcCZpE5tHKI085WoPH9uLrB91J1n0DSumIEu0cA9tqNsWjUSsfUSx9Jxmpu9CHa%2FMzv%2BUNssP%2Bx1iaGzZtRR1Ss535MZWfmsiEgvsB%2BBfbHUBMiT7elorG4QyO02rGOwP8NgIf61Y%2BKsLpPfJuVAPl1o0YM4VRaja%2Fg60N0Us0ZKXfmMD0z9LU%2F85NA8kY0TvudtplmZ7m0toAXyCSu8zworlAV1qO5gEEuXOmwlejVFFP0mYkgwNRVVli07%2BNXv8xlSPPw1m0TM8SX7iyQf0al9wiSoH0VkqWZcQpJ6J2YdcRlOs4CcEihPgTQ4xAtcIBupT%2FQRqtMQh7fzYiJB0Ihh9We7d8TwKqADNyNqpVZ6TiTOCdelWQ36YyhBqZi0t%2FBf6o%2FZvWHgEYAf1MCMY2fKM%2FNMCxDQXr902IOondaJWAg1d8hE0Y4p135gF4KScLOnRbxn7n9u%2FbfrVz0s%3D&key=n6Z8qI2UQFZ3M%2BIPaFgpeuIy4tsr0%2F4a%2F%2FQYfev3m1nW9YuYd1ZXWo9aitVW%2Bg4ba2x%2FIaLCfv6T2%2BC6RQtXAUSEXUs4yvsCOrYUiCru%2FbKAikSUvontF7NkZYohiJMbJIP1zAEIPnN6fY%2Foh0rOL4GtNoeQeWEHqzO3PZsdYIs%3D;
 echo "Jumlah ?\nInput : ";
 $jumlah = trim(fgets(STDIN));
 $submit = id($sign,$data,$body,$jumlah,$wait);
